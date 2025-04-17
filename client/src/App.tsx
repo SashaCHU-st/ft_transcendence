9410ae46c61656a79ae3e7f7ec5758df075c0713
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2025-04-17 14:10:10 +0300

    some profile added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,35 @@
-
-
-import './index.css';
-import Profile from './pages/Profile/Profile';
+// import { useState } from 'react'
+import AppRouter from './router/AppRouter'
+import './App.css'
 
 function App() {
-  return <Profile />;
+  // const [count, setCount] = useState(0)
+
+  return (
+    // <>
+    //   <div>
+    //     <a href="https://vite.dev" target="_blank">
+    //       <img src={viteLogo} className="logo" alt="Vite logo" />
+    //     </a>
+    //     <a href="https://react.dev" target="_blank">
+    //       <img src={reactLogo} className="logo react" alt="React logo" />
+    //     </a>
+    //   </div>
+    //   <h1>Vite + React</h1>
+    //   <div className="card">
+    //     <button onClick={() => setCount((count) => count + 1)}>
+    //       count is {count}
+    //     </button>
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to test HMR
+    //     </p>
+    //   </div>
+    //   <p className="read-the-docs">
+    //     Click on the Vite and React logos to learn more
+    //   </p>
+    // </>
+    <AppRouter/>
+  )
 }
 
-export default App;
+export default App
