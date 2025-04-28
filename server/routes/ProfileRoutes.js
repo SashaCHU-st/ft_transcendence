454d504db34444b67e2a76b5df85d087bf878f16
SashaCHU-st <alexandra.churina@gmail.com>
2025-04-28 13:30:37 +0300

    upload pictures works

--- server/routes/ProfileRoutes.js
+++ server/routes/ProfileRoutes.js
@@ -1,21 +1,35 @@
 import { ProfileSchema } from "../schema/profile.schema.js";
-import { updateProfile } from "../controllers/profile.js";
+import fastifyMultipart from "@fastify/multipart";
+import { updateProfile, uploadPicture } from "../controllers/profile.js";
 
 async function profileRoutes(fastify) {
+  fastify.register(fastifyMultipart);
 
-    fastify.patch("/updateProfile", async (req, reply) =>
-    {
-        const validated = ProfileSchema.safeParse(req.body);
-            if(!validated.success)
-            {
-              return reply.code(400).send({
-                message: "Validation error",
-                errors: validated.error.errors,
-              });
-            }
-            return updateProfile ({...req, body:validated.data}, reply);
-    })
-    
+  fastify.patch("/updateProfile", async (req, reply) => {
+    const validated = ProfileSchema.safeParse(req.body);
+    if (!validated.success) {
+      return reply.code(400).send({
+        message: "Validation error",
+        errors: validated.error.errors,
+      });
+    }
+    return updateProfile({ ...req, body: validated.data }, reply);
+  });
+  fastify.post("/uploadPicture", async (req, reply) => {
+    const pic = await req.file();
+    // const email = pic.fields?.email.value;
+    // console.log("email:", email);
+
+    if (!pic) {
+      return reply.code(400).send({ message: "No picture uploaded" });
+    }
+
+    // if (!email) {
+    //   return reply.code(400).send({ message: "No email provided" });
+    // }
+
+    return uploadPicture(pic, reply);
+  });
 }
 
 export default profileRoutes;
--- server/schema/profile.schema.js
+++ server/schema/profile.schema.js
@@ -6,6 +6,7 @@ export const ProfileSchema = z.object
         name: z.string().max(20),
         nickname: z.string().max(20),
         password: z.string().min(4).max(40),
-        email: z.string().max(40).email(),
+        id: z.string(),
+        // image: z.string(),
     }
 )
