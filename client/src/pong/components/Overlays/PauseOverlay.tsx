f120e3dd75fa7c17b32cfdabc7f5885aeb8a9a6e
Alisa Arbenina <aarbenina@gmail.com>
2025-05-27 09:40:49 +0300

    apply new style to pause screen

--- client/src/pong/components/Overlays/PauseOverlay.tsx
+++ client/src/pong/components/Overlays/PauseOverlay.tsx
@@ -1,4 +1,4 @@
-import { StarryBackground } from "../StarryBackground";
+import { SpaceBackground } from "../SpaceBackground";
 
 interface PauseOverlayProps {
   waitingStart: boolean;
@@ -6,48 +6,50 @@ interface PauseOverlayProps {
 
 export function PauseOverlay({ waitingStart }: PauseOverlayProps) {
   return (
-    <div className={`
-        absolute
-        inset-0
-        z-20
-        flex
-        flex-col
-        items-center
-        justify-center
-        bg-black
-        bg-opacity-50
-      `}
-    >
-      <StarryBackground />
-      {waitingStart ? (
-        <div className="space-y-4 text-center">
-          <div className={`
+    <SpaceBackground>
+      <div
+        className="
+          relative
+          z-10
+          flex
+          items-center
+          justify-center
+          h-full
+          w-full
+        "
+      >
+        {waitingStart ? (
+          <div className="space-y-4 text-center">
+            <div
+              className="
+                text-4xl
+                font-bold
+                text-cyan-300
+                text-shadow-[0_0_4px_rgba(0,255,255,0.6)]
+              "
+            >
+              Press any key to start
+            </div>
+            <div className="text-lg text-white">
+              <p>Controls:</p>
+              <p>Player 1: W / S</p>
+              <p>Player 2: ↑ / ↓</p>
+              <p>Single player: ↑ / ↓</p>
+            </div>
+          </div>
+        ) : (
+          <div
+            className="
               text-4xl
               font-bold
               text-cyan-300
               text-shadow-[0_0_4px_rgba(0,255,255,0.6)]
-            `}
+            "
           >
-            Press any key to start
+            PAUSED
           </div>
-          <div className="text-lg text-white">
-            <p>Controls:</p>
-            <p>Player 1: W / S</p>
-            <p>Player 2: ↑ / ↓</p>
-            <p>Single player: ↑ / ↓</p>
-          </div>
-        </div>
-      ) : (
-        <div className={`
-            text-4xl
-            font-bold
-            text-cyan-300
-            text-shadow-[0_0_4px_rgba(0,255,255,0.6)]
-          `}
-        >
-          PAUSED
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </SpaceBackground>
   );
 }
