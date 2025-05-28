cf456782f741df446977eb496d8d33ec46ac5405
SashaCHU-st <alexandra.churina@gmail.com>
2025-05-28 15:20:17 +0300

    added delete from fav

--- server/routes/FavoritesRoutes.js
+++ server/routes/FavoritesRoutes.js
@@ -1,5 +1,5 @@
 import { favoritesSchema } from "../schema/favorites.schema.js";
-import { addfavorites } from "../controllers/favorites.js";
+import { addfavorites, deletefavorites } from "../controllers/favorites.js";
 import { validatedValues } from "../utils/validate.js";
 
 async function favoriteRoutes(fastify) {
@@ -7,6 +7,12 @@ async function favoriteRoutes(fastify) {
     const validated = favoritesSchema.safeParse(req.body);
     const data =await validatedValues(validated, reply);
     return addfavorites({ ...req, body: data }, reply);
+  });
+
+    fastify.delete("/deletefavorites", async (req, reply) => {
+    const validated = favoritesSchema.safeParse(req.body);
+    const data =await validatedValues(validated, reply);
+    return deletefavorites({ ...req, body: data }, reply);
   });
 }
 
