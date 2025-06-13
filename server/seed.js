67f8a0f58f04e96ded06e69ce921f485d4f0eb79
SashaCHU-st <alexandra.churina@gmail.com>
2025-06-13 10:40:56 +0300

    made bubble chat for receved message left corner sharp; creating game statistics for users with dates, who lose who win in challenge

--- server/seed.js
+++ server/seed.js
@@ -3,6 +3,8 @@ import Database from "better-sqlite3";
 const db = new Database("./database/database.db");
 
 db.exec(
-  `DELETE FROM challenge WHERE  id < 60
+  `
+  DELETE FROM game WHERE  id < 100;
+  DELETE FROM challenge WHERE  id < 100;
   ` 
 );
