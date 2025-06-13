57d9146b5701394a37a0ef61eef740dce07101ce
SashaCHU-st <alexandra.churina@gmail.com>
2025-06-13 11:31:15 +0300

    creating gama table based on challange; printing out statistic with dates

--- server/controllers/statistics.js
+++ server/controllers/statistics.js
@@ -1,7 +1,7 @@
-import db from "../database/database.js";
+import db from '../database/database.js';
 
 export async function statisticsAll(request, reply) {
-  const stat = db.prepare(`SELECT id,nickname, wins, losses FROM users`).all();
+  const stat = db.prepare(`SELECT id, challenge_id, win_user_id, losses_user_id, date FROM game`).all();
 
   return reply.code(200).send({ stat });
 }
@@ -13,64 +13,76 @@ export async function statisticsUser(req, reply) {
     const statUser = db
       .prepare(`SELECT * FROM users WHERE id = ?`)
       .get(user_id);
-    console.log("USER STAT =>", statUser.wins);
+    console.log('USER STAT =>', statUser.wins);
 
-    const wins = statUser.wins
-    const losses = statUser.losses
+    const wins = statUser.wins;
+    const losses = statUser.losses;
 
-    return reply.code(200).send({ "wins":wins, "losses": losses});
+    return reply.code(200).send({ wins: wins, losses: losses });
   } catch (err) {
-    console.error("Database error:", err.message);
-    return reply.code(500).send({ message: "Something went wrong" });
+    console.error('Database error:', err.message);
+    return reply.code(500).send({ message: 'Something went wrong' });
   }
 }
 
 export async function win(req, reply) {
-  console.log("WE IN WIN");
+  console.log('WE IN WIN');
 
   const { user_id } = req.body;
 
   try {
+    const challenge_id = 3;
     const gameEND = db
-  .prepare(`UPDATE game SET win_user_id = ?, date = ? WHERE challenge_id = ? `)
-  .run(user_id,new Date().toISOString(),1);
-    const winUser = db.prepare(`SELECT * FROM users WHERE id = ?`).get(user_id);
+      .prepare(
+        `UPDATE game SET win_user_id = ?, date = ? WHERE challenge_id = ? `
+      )
+      .run(user_id, new Date().toISOString(), challenge_id);
+      console.log("JJJJJJ=>", gameEND)
+    if (gameEND.changes !== 0) {
+      const winUser = db
+        .prepare(`SELECT * FROM users WHERE id = ?`)
+        .get(user_id);
 
-    const winValue = winUser.wins + 1;
-    const updateWins = db.prepare(`UPDATE users SET wins = ? WHERE id = ?`).run(winValue, user_id);
-
-    console.log("YYYYY=>", user_id)
-
-    console.log("LLLL=>", gameEND)
-    return reply.code(200).send({ updateWins, gameEND });
+      const winValue = winUser.wins + 1;
+      const updateWins = db
+        .prepare(`UPDATE users SET wins = ? WHERE id = ?`)
+        .run(winValue, user_id);
+      return reply.code(200).send({ updateWins, gameEND });
+    } else {
+      return reply.code(400).send({ message: 'We are not in game' });
+    }
   } catch (err) {
-    console.error("Database error:", err.message);
-    return reply.code(500).send({ message: "Something went wrong" });
+    console.error('Database error:', err.message);
+    return reply.code(500).send({ message: 'Something went wrong' });
   }
 }
 
 export async function loseUser(req, reply) {
-    const { user_id } = req.body;
-  
-    try {
-      const gameEND = db
-    .prepare(`UPDATE game SET losses_user_id = ?, date = ? WHERE challenge_id = ? `)
-    .run(user_id,new Date().toISOString(),1);
-      const loseUser = db.prepare(`SELECT * FROM users WHERE id = ?`).get(user_id);
+  const { user_id } = req.body;
+
+  try {
+    const challenge_id =3;
+    const gameEND = db
+      .prepare(
+        `UPDATE game SET losses_user_id = ?, date = ? WHERE challenge_id = ? `
+      )
+      .run(user_id, new Date().toISOString(), challenge_id);
+    if (gameEND.changes !== 0) {
+      const loseUser = db
+        .prepare(`SELECT * FROM users WHERE id = ?`)
+        .get(user_id);
 
       const count = loseUser.losses + 1;
       const updateLoses = db
         .prepare(`UPDATE users SET losses = ? WHERE id = ?`)
         .run(count, user_id);
-  
 
-    console.log("YYYYY=>", user_id)
-
-    console.log("LLLL=>", gameEND)
       return reply.code(200).send({ updateLoses, gameEND });
-    } catch (err) {
-      console.error("Database error:", err.message);
-      return reply.code(500).send({ message: "Something went wrong" });
+    } else {
+      return reply.code(400).send({ message: 'We are not in game' });
     }
+  } catch (err) {
+    console.error('Database error:', err.message);
+    return reply.code(500).send({ message: 'Something went wrong' });
   }
-  
+}
