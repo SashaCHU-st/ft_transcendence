2650cfad92e96e2d0421e1f795b70ebe94fee914
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2025-06-26 14:18:11 +0300

    fixed full hostory for own info

--- server/seed.js
+++ server/seed.js
@@ -4,11 +4,11 @@ const db = new Database("./database/database.db");
 
 db.exec(
   `
-  DELETE FROM game WHERE  id < 100;
-  DELETE FROM challenge WHERE  id <100;
+  DELETE FROM friends WHERE  id < 100;
   ` 
 );
 
 // UPDATE challenge SET game_end = 1 WHERE id = 3;
 // DELETE FROM challenge WHERE  id < 100;
 // UPDATE game SET losses_user_id = 1, win_user_id = 2, lose_score = 2, win_score = 30, lose_score = 15 WHERE id = 8;
+// DELETE FROM challenge WHERE  id <100;
