c8c0e18d0bbf6f5392eb97f505185125124fd62c
Alisa Arbenina <aarbenina@gmail.com>
2025-06-27 08:56:18 +0300

    game: add skybox; add thickness to playing field

--- client/src/pong/components/Overlays/EscMenu.tsx
+++ client/src/pong/components/Overlays/EscMenu.tsx
@@ -8,7 +8,12 @@ interface EscMenuProps {
   onMenuAction: (index: number) => void;
 }
 
-export function EscMenu({ menuItems, menuIndex, setMenuIndex, onMenuAction }: EscMenuProps) {
+export function EscMenu({
+  menuItems,
+  menuIndex,
+  setMenuIndex,
+  onMenuAction,
+}: EscMenuProps) {
   useEffect(() => {
     function handleKey(e: KeyboardEvent) {
       if (e.key === "ArrowUp") {
@@ -28,33 +33,27 @@ export function EscMenu({ menuItems, menuIndex, setMenuIndex, onMenuAction }: Es
 
   const styles: { base: string; active: string }[] = [
     {
-      base:
-        "border-[#0A7FC9] text-[#D3E0FB] shadow-[0_0_15px_rgba(0,255,255,0.7)] hover:bg-cyan-900 hover:bg-opacity-30",
+      base: "border-[#0A7FC9] text-[#D3E0FB] shadow-[0_0_15px_rgba(0,255,255,0.7)] hover:bg-cyan-900 hover:bg-opacity-30",
       active: "bg-cyan-900 bg-opacity-30",
     },
     {
-      base:
-        "border-[#BD0E86] text-[#832264] shadow-[0_0_15px_rgba(255,29,153,0.7)] hover:bg-pink-900 hover:bg-opacity-30",
+      base: "border-[#BD0E86] text-[#832264] shadow-[0_0_15px_rgba(255,29,153,0.7)] hover:bg-pink-900 hover:bg-opacity-30",
       active: "bg-pink-900 bg-opacity-30",
     },
     {
-      base:
-        "border-[#9010CE] text-[#743b91] shadow-[0_0_12px_rgba(192,38,211,0.7)] hover:bg-purple-900 hover:bg-opacity-30",
+      base: "border-[#9010CE] text-[#743b91] shadow-[0_0_12px_rgba(192,38,211,0.7)] hover:bg-purple-900 hover:bg-opacity-30",
       active: "bg-purple-900 bg-opacity-30",
     },
     {
-      base:
-        "border-[#74C0FC] text-[#297db1] shadow-[0_0_15px_rgba(74,192,252,0.7)] hover:bg-blue-900 hover:bg-opacity-30",
+      base: "border-[#74C0FC] text-[#297db1] shadow-[0_0_15px_rgba(74,192,252,0.7)] hover:bg-blue-900 hover:bg-opacity-30",
       active: "bg-blue-900 bg-opacity-30",
     },
     {
-      base:
-        "border-[#74C0FC] text-[#297db1] shadow-[0_0_15px_rgba(74,192,252,0.7)] hover:bg-blue-900 hover:bg-opacity-30",
+      base: "border-[#74C0FC] text-[#297db1] shadow-[0_0_15px_rgba(74,192,252,0.7)] hover:bg-blue-900 hover:bg-opacity-30",
       active: "bg-blue-900 bg-opacity-30",
     },
     {
-      base:
-        "border-[#BD0E86] text-[#832264] shadow-[0_0_15px_rgba(255,29,153,0.7)] hover:bg-pink-900 hover:bg-opacity-30",
+      base: "border-[#BD0E86] text-[#832264] shadow-[0_0_15px_rgba(255,29,153,0.7)] hover:bg-pink-900 hover:bg-opacity-30",
       active: "bg-pink-900 bg-opacity-30",
     },
   ];
@@ -69,7 +68,9 @@ export function EscMenu({ menuItems, menuIndex, setMenuIndex, onMenuAction }: Es
               key={idx}
               onMouseEnter={() => setMenuIndex(idx)}
               onClick={() => onMenuAction(idx)}
-              className={`menu-btn w-[400px] py-4 text-lg font-bold border-2 rounded-xl bg-black bg-opacity-10 transition-transform duration-200 hover:scale-105 ${style.base} ${menuIndex === idx ? style.active + ' scale-105' : ''}`}
+              className={`menu-btn w-[400px] py-4 text-lg font-bold border-2 rounded-xl bg-black bg-opacity-10 transition-transform duration-200 hover:scale-105 ${
+                style.base
+              } ${menuIndex === idx ? style.active + " scale-105" : ""}`}
             >
               {text}
             </button>
@@ -79,4 +80,3 @@ export function EscMenu({ menuItems, menuIndex, setMenuIndex, onMenuAction }: Es
     </OverlayWrapper>
   );
 }
-
