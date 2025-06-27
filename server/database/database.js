0f6fe75089a3958b48ef56595ab4d9d0d5d4252b
Aleksandra Heinanen <aleksandra.heinanen@gmail.com>
2025-06-27 12:15:58 +0300

    cors works

--- server/database/database.js
+++ server/database/database.js
@@ -35,15 +35,7 @@ db.exec(`
   );
  `);
  
-db.exec(`
-  CREATE TABLE IF NOT EXISTS favorites (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    username TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (username) REFERENCES users(username)
-  );
-`);
+
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS challenge (
